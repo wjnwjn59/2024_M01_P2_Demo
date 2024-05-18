9d416d227b50cc58905360a8c286b4c50403f183
wjnwjn59 <thangduong.no01@gmail.com>
2024-05-18 14:51:23 +0700

    update onnx version

--- utils.py
+++ utils.py
@@ -4,19 +4,17 @@ import gdown
 import zipfile
 
 def download_model():
-    url = 'https://drive.google.com/file/d/1_UcxxRSLh5QAZf5BQvNsSgQilWpnPq7F/view?usp=sharing'
+    url = 'https://drive.google.com/file/d/1jl4TmG5l9ETpTs3sH4BHyLayXWRMc7E1/view?usp=sharing'
     output_file = 'models/yolov9/weights/model_weight.zip'
     unzip_dest = 'models/yolov9/weights'
     os.makedirs(unzip_dest, exist_ok=True)
 
     gdown.download(url, 
                    output_file, 
-                   quiet=True,
-                   fuzzy=True)
+                   quiet=True)
 
     with zipfile.ZipFile(output_file, 'r') as zip_ref:
         zip_ref.extractall(unzip_dest)
-
 
 def generate_name():
     uuid_str = str(uuid.uuid4())
