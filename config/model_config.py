37b578f5209b0d195a6c15c86b9fc1ac7e378aa9
wjnwjn59 <thangduong.no01@gmail.com>
2024-06-28 04:27:27 +0700

    change new config for yolov10

--- config/model_config.py
+++ config/model_config.py
@@ -1,3 +1,4 @@
 class Detector_Config:
-    weight_path: str = 'models/yolov9/weights/best.onnx'
-    yaml_path: str = 'models/yolov9/human_data.yaml'
+    origin_weight_path: str = 'models/yolov10/weights/helmet_safety_best.pt'
+    export_weight_path: str = 'models/yolov10/weights/helmet_safety_best.onnx'
+    yaml_path: str = 'models/yolov10/helmet_safety.yaml'
